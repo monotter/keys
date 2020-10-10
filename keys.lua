57d721924f31bedbb6fe48a2a8e7a8f7db93dbe3
tututuana <51187395+tututuana@users.noreply.github.com>
2020-10-10 16:42:31 +0300

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,9 +1,3 @@
 local WhitelistedIP = {
     "46.197.116.90"
 }
-local IP = game:HttpGet("https://api.ipify.org")
-if table.find(WhitelistedIP,IP) then
---if whitelisted
-else
---if not whitelisted
-end
